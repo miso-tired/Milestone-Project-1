03e3bc7f4a26e4357b7538c30542ccd299065824
Anthony <anthonylsa12@gmail.com>
2024-01-08 20:39:53 -0800

    Snake can now move with arrow keys

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const playBoard = document.querySelector('.play-board');
 
 let nodeX, nodeY;
 let snakeX = 5, snakeY = 10;
+let velocityX = 0, velocityY = 0;
 
 const changeNodePosition = () => {
     nodeX = Math.floor(Math.random() * 35) + 1;
@@ -11,11 +12,28 @@ const changeNodePosition = () => {
 }
 
 const changeDirection = (e) => {
-    console.log(e);
+    if(e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    } else if(e.key === "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    } else if(e.key === "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    } else if(e.key === "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
+    startGame()
 }
 
 const startGame = () => {
     let htmlMarkup = `<div class="node" style="grid-area: ${nodeY} / ${nodeX}"></div>`;
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
     htmlMarkup += `<div class ="snake" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
