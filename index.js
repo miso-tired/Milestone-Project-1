db477adab56ec7f3715b1e09e0662744973ce9ab
Anthony <anthonylsa12@gmail.com>
2024-01-08 19:26:20 -0800

    made snake node points and allowed randomization of their positions

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 /* Make Snake */
+const playBoard = document.querySelector('.play-board');
 
-/* Make nodes for snake to eat */
+let nodeX = 10, nodeY = 15;
+let snakeX = 5, snakeY = 10;
+
+const changeNodePosition = () => {
+    nodeX = Math.floor(Math.random() * 35) + 1;
+    nodeY = Math.floor(Math.random() * 35) + 1;
+
+}
+
+const startGame = () => {
+    let htmlMarkup = `<div class="node" style="grid-area: ${nodeY} / ${nodeX}"></div>`;
+    htmlMarkup += `<div class ="snake" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
+}
+
+changeNodePosition();
+startGame();
