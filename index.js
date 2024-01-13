868b09a1b42a0d9baa2ceb587f21e820f215227b
Anthony <anthonylsa12@gmail.com>
2024-01-12 21:08:35 -0800

    Added game over

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 /* Make Snake */
 const playBoard = document.querySelector('.play-board');
 
+
+let gameOver = false; 
 let nodeX, nodeY;
 let snakeX = 5, snakeY = 10;
 let snakeBody = [];
 let velocityX = 0, velocityY = 0;
+let setIntervalId;
 
 const changeNodePosition = () => {
     nodeX = Math.floor(Math.random() * 35) + 1;
     nodeY = Math.floor(Math.random() * 35) + 1;
+}
+
+const promptGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over! Press OK to replay");
+    location.reload();
 }
 
 const changeDirection = (e) => {
@@ -29,6 +38,7 @@ const changeDirection = (e) => {
 }
 
 const startGame = () => {
+    if(gameOver) return promptGameOver();
     let htmlMarkup = `<div class="node" style="grid-area: ${nodeY} / ${nodeX}"></div>`;
 
     if(snakeX === nodeX && snakeY === nodeY) {
@@ -46,6 +56,10 @@ const startGame = () => {
     snakeX += velocityX;
     snakeY += velocityY;
 
+    if(snakeX <= 0 || snakeX > 35 || snakeY <= 0 || snakeY > 35) {
+        gameOver = true;
+    }
+
     for (let i = 0; i < snakeBody.length; i++) {
         htmlMarkup += `<div class ="snake" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
     }
@@ -54,6 +68,6 @@ const startGame = () => {
 }
 
 changeNodePosition();
-setInterval(startGame, 125);
+setIntervalId = setInterval(startGame, 125);
 
 document.addEventListener("keydown", changeDirection);
